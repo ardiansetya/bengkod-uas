cf24605279d65577d04dc57cd9813c7acf815131
ardiansetya <ardiansetya129@gmail.com>
2025-06-07 11:23:24 +0700

    obat model done

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Obat extends Model
 {
-    //
+    use HasFactory;
+
+    // Nama tabel (opsional jika sesuai konvensi)
+    protected $table = 'obats';
+
+    // Kolom yang bisa diisi massal
+    protected $fillable = [
+        'nama_obat',
+        'kemasan',
+        'harga',
+    ];
+
+    public function detailPeriksa(){
+        return $this->hasMany(DetailPeriksa::class, 'id_obat');
+    }
 }
