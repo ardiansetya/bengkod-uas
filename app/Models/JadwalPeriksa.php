fa48256ec86f2dc7cf30b5435507f82aeddaf576
ardiansetya <ardiansetya129@gmail.com>
2025-06-07 11:18:00 +0700

    model jadwalperiksa done

--- app/Models/JadwalPeriksa.php
+++ app/Models/JadwalPeriksa.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class JadwalPeriksa extends Model
 {
-    //
+    use HasFactory;
+
+    // Tentukan nama tabel jika berbeda dari konvensi Laravel
+    protected $table = 'jadwal_periksas';
+
+    // Tentukan kolom yang bisa diisi
+    protected $fillable = [
+        'id_dokter',
+        'hari',
+        'jam_mulai',
+        'jam_selesai',
+    ];
+
+    public function daftarPoli()
+    {
+        return $this->hasMany(DaftarPoli::class, 'id_jadwal');
+    }
+
+    public function dokter()
+    {
+        return $this->belongsTo(User::class, 'id_dokter');
+    }
 }
