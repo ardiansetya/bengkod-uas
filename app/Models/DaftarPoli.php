20a99a45c8b06010c9837b2da80c7072be5d813c
ardiansetya <ardiansetya129@gmail.com>
2025-06-07 11:24:57 +0700

    model daftar poli done

--- app/Models/DaftarPoli.php
+++ app/Models/DaftarPoli.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DaftarPoli extends Model
 {
-    //
+    use HasFactory;
+
+    // Nama tabel (opsional, sesuai konvensi)
+    protected $table = 'daftar_polis';
+
+    // Kolom yang dapat diisi secara massal
+    protected $fillable = [
+        'id_pasien',
+        'id_jadwal',
+        'keluhan',
+        'no_antrian',
+    ];
+
+    public function periksa(){
+        return $this->hasMany(Periksa::class, 'id_daftar_poli');
+    }
 }
