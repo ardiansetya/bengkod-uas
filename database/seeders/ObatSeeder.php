9335824f0e6be21d542fbaaafda42ba1f507fce9
ardiansetya <ardiansetya129@gmail.com>
2025-06-07 11:48:56 +0700

    obat seeder done

--- database/seeders/ObatSeeder.php
+++ database/seeders/ObatSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Obat;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class ObatSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Obat::insert([
+            [
+                'nama_obat' => 'Paracetamol',
+                'kemasan' => 'Tablet 500mg',
+                'harga' => 5000
+            ],
+            [
+                'nama_obat' => 'OBH Combi',
+                'kemasan' => 'Botol 60ml',
+                'harga' => 15000
+            ]
+        ]);
     }
 }
