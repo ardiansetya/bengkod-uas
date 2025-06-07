d86b80273d2c149248469b0d8647de544525e4f9
ardiansetya <ardiansetya129@gmail.com>
2025-06-07 11:48:29 +0700

    jadwal periksa seeder done

--- database/seeders/JadwalPeriksaSeeder.php
+++ database/seeders/JadwalPeriksaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\JadwalPeriksa;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,11 @@ class JadwalPeriksaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        JadwalPeriksa::create([
+            'id_dokter' => 1,
+            'hari' => 'senin',
+            'jam_mulai' => '08:00:00',
+            'jam_selesai' => '10:00:00'
+        ]);
     }
 }
