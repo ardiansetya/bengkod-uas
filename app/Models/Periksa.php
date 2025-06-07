20220043610a5f20063255b7c6a2e3c15b50b01c
ardiansetya <ardiansetya129@gmail.com>
2025-06-07 11:22:04 +0700

    periksa model done

--- app/Models/Periksa.php
+++ app/Models/Periksa.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Periksa extends Model
 {
-    //
+    protected $table = 'periksas';
+
+    // Kolom yang dapat diisi secara massal
+    protected $fillable = [
+        'id_daftar_poli',
+        'tanggal_periksa',
+        'catatan',
+        'biaya_periksa',
+    ];
+
+
+    public function detailPeriksa(){
+        return $this->hasMany(DetailPeriksa::class, 'id_periksa');
+    }
 }
