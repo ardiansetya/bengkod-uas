804de5dd45c8911f265d2c73b635758001661d4f
ardiansetya <ardiansetya129@gmail.com>
2025-06-07 11:20:30 +0700

    poli model done

--- app/Models/Poli.php
+++ app/Models/Poli.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Poli extends Model
 {
-    //
+    use HasFactory;
+
+    // Nama tabel (opsional jika sesuai konvensi)
+    protected $table = 'polis';
+
+    // Kolom yang dapat diisi secara massal
+    protected $fillable = [
+        'nama_poli',
+        'keterangan',
+    ];
+
+    public function dokter(){
+        return $this->hasMany(User::class, 'id_poli');
+    }
+    
 }
